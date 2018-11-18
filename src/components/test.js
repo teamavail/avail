c97e1944baa4837a8169f63b1c3f4322b1966f8d
howardcsps <howardcsps@gmail.com>
2018-11-17 19:17:28 -0800

    starting landing page

--- src/components/test.js
+++ src/components/test.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 class Test extends Component {
   render() {
     return (
+<div>
       <h1> Avail! </h1>
+      <h2> Profile </h2>
+      </div>
     )
   }
 }
